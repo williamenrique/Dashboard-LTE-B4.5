2b59c16c8d583451c91916b4cebef28fb0863ec9
williamenrique <william21enrique@gmail.com>
2020-10-21 19:14:02 -0400

    template

--- system/app/Views/ModulesAdmin/template.php
+++ system/app/Views/ModulesAdmin/template.php
@@ -8,11 +8,10 @@
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0"><?= $controller?></h1>
 				</div><!-- /.col -->
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
-						<li class="breadcrumb-item"><a href="#">Home</a></li>
+						<li class="breadcrumb-item"><a href="<?= base_url()?>dashboard">Home</a></li>
 						<li class="breadcrumb-item">Usuarios</li>
 					</ol>
 				</div><!-- /.col -->
