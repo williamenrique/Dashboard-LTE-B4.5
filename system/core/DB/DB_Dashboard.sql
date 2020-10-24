d7535077fc55372c6da170aa2451f6ce8f6b6a47
williamenrique <william21enrique@gmail.com>
2020-10-24 11:45:45 -0400

    DB_dashboard

--- system/core/DB/DB_Dashboard.sql
+++ system/core/DB/DB_Dashboard.sql
@@ -18,33 +18,6 @@ USE `db_dashboard`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `table_cargo`
---
-
-DROP TABLE IF EXISTS `table_cargo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `table_cargo` (
-  `cargo_id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `descripcion` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `fecha_creacion` date DEFAULT NULL,
-  PRIMARY KEY (`cargo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `table_cargo`
---
-
-LOCK TABLES `table_cargo` WRITE;
-/*!40000 ALTER TABLE `table_cargo` DISABLE KEYS */;
-INSERT INTO `table_cargo` VALUES (1,'Administrador','Administrador del sistema',1,NULL),(2,'Encargado','Personal asistente',1,NULL),(3,'Cliente','Persona ajena al sistema',1,NULL);
-/*!40000 ALTER TABLE `table_cargo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `table_log`
 --
 
@@ -60,7 +33,7 @@ CREATE TABLE `table_log` (
   PRIMARY KEY (`log_id`),
   KEY `log_idUser_idx` (`log_idUser`),
   CONSTRAINT `log_idUser` FOREIGN KEY (`log_idUser`) REFERENCES `table_user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +42,7 @@ CREATE TABLE `table_log` (
 
 LOCK TABLES `table_log` WRITE;
 /*!40000 ALTER TABLE `table_log` DISABLE KEYS */;
-INSERT INTO `table_log` VALUES (1,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 20',NULL),(2,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 19',NULL),(3,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 21',NULL),(4,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 18',NULL),(5,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 16',NULL),(6,1,'/index.php22','INSERT INTO table_user(user_ci,user_nombres,user_apellidos, user_tlf,user_email,user_status,user_img, user_rol,user_pass) VALUES(?,?,?,?,?,?,?,?,?)',NULL),(7,1,'/index.php23','INSERT INTO table_user(user_ci,user_nombres,user_apellidos, user_tlf,user_email,user_status,user_img, user_rol,user_pass) VALUES(?,?,?,?,?,?,?,?,?)',NULL),(8,1,'/index.php24','INSERT INTO table_user(user_ci,user_nombres,user_apellidos, user_tlf,user_email,user_status,user_img, user_rol,user_pass) VALUES(?,?,?,?,?,?,?,?,?)',NULL);
+INSERT INTO `table_log` VALUES (1,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 20',NULL),(2,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 19',NULL),(3,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 21',NULL),(4,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 18',NULL),(5,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 16',NULL),(6,1,'/index.php22','INSERT INTO table_user(user_ci,user_nombres,user_apellidos, user_tlf,user_email,user_status,user_img, user_rol,user_pass) VALUES(?,?,?,?,?,?,?,?,?)',NULL),(7,1,'/index.php23','INSERT INTO table_user(user_ci,user_nombres,user_apellidos, user_tlf,user_email,user_status,user_img, user_rol,user_pass) VALUES(?,?,?,?,?,?,?,?,?)',NULL),(8,1,'/index.php24','INSERT INTO table_user(user_ci,user_nombres,user_apellidos, user_tlf,user_email,user_status,user_img, user_rol,user_pass) VALUES(?,?,?,?,?,?,?,?,?)',NULL),(9,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 24',NULL),(10,1,'/index.php1','UPDATE table_user SET user_status = ? WHERE user_id = 23',NULL);
 /*!40000 ALTER TABLE `table_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +62,7 @@ CREATE TABLE `table_menu` (
   `page_menu_open` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   `page_link` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +71,7 @@ CREATE TABLE `table_menu` (
 
 LOCK TABLES `table_menu` WRITE;
 /*!40000 ALTER TABLE `table_menu` DISABLE KEYS */;
-INSERT INTO `table_menu` VALUES (1,'Usuario',NULL,NULL,1,'usuario','usuarios'),(2,'Menu',NULL,NULL,1,'menu','menus'),(3,'Timeline',NULL,NULL,1,'time','times');
+INSERT INTO `table_menu` VALUES (1,'Usuario',NULL,'far fa-user',1,'usuario','usuarios'),(2,'Menu',NULL,'fas fa-list-ul',1,'menu','menus'),(3,'Timeline',NULL,'far fa-clock',1,'timelines','times'),(4,NULL,NULL,'',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `table_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +87,7 @@ CREATE TABLE `table_menu_sub_menu` (
   `id_menu` int(11) DEFAULT NULL,
   `id_sub_menu` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_menu_sub_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,7 +96,7 @@ CREATE TABLE `table_menu_sub_menu` (
 
 LOCK TABLES `table_menu_sub_menu` WRITE;
 /*!40000 ALTER TABLE `table_menu_sub_menu` DISABLE KEYS */;
-INSERT INTO `table_menu_sub_menu` VALUES (1,1,1),(2,1,2),(3,1,3),(4,2,4),(5,3,5),(8,1,5);
+INSERT INTO `table_menu_sub_menu` VALUES (1,1,1),(2,1,2),(3,1,3),(4,2,4),(5,3,5),(15,2,8);
 /*!40000 ALTER TABLE `table_menu_sub_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,7 +113,7 @@ CREATE TABLE `table_roles` (
   `rol_descripcion` text COLLATE utf8_spanish_ci,
   `rol_status` int(11) DEFAULT NULL,
   PRIMARY KEY (`rol_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,7 +122,7 @@ CREATE TABLE `table_roles` (
 
 LOCK TABLES `table_roles` WRITE;
 /*!40000 ALTER TABLE `table_roles` DISABLE KEYS */;
-INSERT INTO `table_roles` VALUES (1,'Administrador','administrador',1),(2,'Encargado','encargado del sistema',1),(3,'Secretaria','encargada de las citas',1);
+INSERT INTO `table_roles` VALUES (1,'Administrador','administrador',1),(2,'Encargado','encargado del sistema',2),(3,'Secretaria','encargada de las citas',2),(4,'repartidor','Encargado de los envios',2);
 /*!40000 ALTER TABLE `table_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +138,7 @@ CREATE TABLE `table_roles_sub_menu` (
   `id_rol` int(11) DEFAULT NULL,
   `id_sub_menu` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_rol_sub_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,7 +147,7 @@ CREATE TABLE `table_roles_sub_menu` (
 
 LOCK TABLES `table_roles_sub_menu` WRITE;
 /*!40000 ALTER TABLE `table_roles_sub_menu` DISABLE KEYS */;
-INSERT INTO `table_roles_sub_menu` VALUES (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5),(13,2,2),(14,2,3);
+INSERT INTO `table_roles_sub_menu` VALUES (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5),(16,1,8);
 /*!40000 ALTER TABLE `table_roles_sub_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +166,7 @@ CREATE TABLE `table_sub_menu` (
   `status` int(11) DEFAULT NULL,
   `page_link_activo` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`id_sub_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +175,7 @@ CREATE TABLE `table_sub_menu` (
 
 LOCK TABLES `table_sub_menu` WRITE;
 /*!40000 ALTER TABLE `table_sub_menu` DISABLE KEYS */;
-INSERT INTO `table_sub_menu` VALUES (1,'Roles',NULL,'roles',1,'roles'),(2,'Lista de Usuarios',NULL,'usuarios',1,'user'),(3,'Usuarios de Alta',NULL,'Usuarios/alta',1,'alta'),(4,'Asignar menu',NULL,'menu',1,'menu'),(5,'Timeline',NULL,'timeline',1,'time');
+INSERT INTO `table_sub_menu` VALUES (1,'Roles',NULL,'roles',1,'roles'),(2,'Lista de Usuarios',NULL,'usuarios',1,'user'),(3,'Usuarios de Alta',NULL,'Usuarios/alta',1,'alta'),(4,'Asignar menu',NULL,'menu',1,'menu'),(5,'Timeline',NULL,'timeline',1,'time'),(8,'Lista menu',NULL,'Menu/lista',1,'lista');
 /*!40000 ALTER TABLE `table_sub_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -221,7 +194,7 @@ CREATE TABLE `table_timeline` (
   `time_inicio` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   `time_fin` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   PRIMARY KEY (`time_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -230,7 +203,6 @@ CREATE TABLE `table_timeline` (
 
 LOCK TABLES `table_timeline` WRITE;
 /*!40000 ALTER TABLE `table_timeline` DISABLE KEYS */;
-INSERT INTO `table_timeline` VALUES (2,1,'biCod-1-0r7u','2020-10-19','04:15:58','0'),(3,2,'biCod-2-cMg5','2020-10-19','05:23:01','05:25:13'),(4,1,'biCod-1-wrZY','2020-10-19','05:25:25',NULL);
 /*!40000 ALTER TABLE `table_timeline` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -265,7 +237,7 @@ CREATE TABLE `table_user` (
 
 LOCK TABLES `table_user` WRITE;
 /*!40000 ALTER TABLE `table_user` DISABLE KEYS */;
-INSERT INTO `table_user` VALUES (1,14607920,'we21','SWRrSEFnUHpvYm1kRVg4bVBjTEZKQT09','William Enrique','Infante Parra','william@gmail.com','4125181629',1,1,NULL,'2020-10-08 22:11:07',NULL),(2,15769775,'ybet','SWRrSEFnUHpvYm1kRVg4bVBjTEZKQT09','Ybet Nacari','Acosta','ybet.naca@gmail.com','4120586489',3,1,NULL,'2020-10-08 22:14:15',NULL),(3,12345,'migue','SWRrSEFnUHpvYm1kRVg4bVBjTEZKQT09','Miguel Jose','Romero','romero@hotmail.com','12345678',2,1,NULL,'2020-10-18 20:11:13',NULL),(14,157697755,'YA-014','SWRrSEFnUHpvYm1kRVg4bVBjTEZKQT09','Ybet','Acosta','acosta@hotmail.com','345678',1,1,NULL,'2020-10-18 20:12:31',NULL),(15,123456,'JR-015','SWRrSEFnUHpvYm1kRVg4bVBjTEZKQT09','Jose Gregorio','Rengel','rengel@gmail.com','2345678',2,1,'default.png','2020-10-18 20:11:13',NULL),(16,34567,'HU-016','bnp4T0JTMDM2UmxETlllb2RGZmVLdz09','Hgygbyug','Uugug','gg@j.com','3456789',1,0,'default.png','2020-10-17 22:53:32',NULL),(17,34567545,'HU-017','YWpDOG9sL3M4aitFWHIzK1FRdTROUT09','Hgygbyug','Uugug','g4g@j.com','3456789',1,0,'default.png','2020-10-17 17:24:36',NULL),(18,9999999,'JB-018','OEJJc1hTVmRQd1l3ZWM4eFB4YVVVdz09','Jbhjbhjb','Bbub','h@jk.com','56789',1,0,'default.png','2020-10-17 22:53:28',NULL),(19,0,'UY-019','UmsyU3hkWnE4KzJLeWZyY0FjNW9VQT09','Ugyy','Ygyg','g@g.com','4567890',1,0,'default.png','2020-10-17 18:04:02',NULL),(20,111111,'BH-020','U1hueUswZ2dmZUQyc3Q0aTdqa2NNdz09','Bhb','Hvhv','hhhhhhh@q.com','456789',1,0,'default.png','2020-10-17 18:03:55',NULL),(21,798778,'KJ-021','QmNVTSt2TjhNUnE4UXNmSlRablBkQT09','Knjb','Jbhjbhb','r@h.com','465',1,0,'default.png','2020-10-17 22:53:24',NULL),(22,67898,'JG-022','UVBma093WEdZeEFtU2l5S1ZTT0Q1dz09','Jesus Enrique','Gilberto','gil@gmail.com','876545678',3,1,'default.png','2020-10-18 00:12:15',NULL),(23,6765544,'OH-023','ZWVoMXZqVkp6dm5QRFhHVFZnUHdtUT09','Oihiuhiuh Nihihi','Hhiuh','ghghg@hotmail.com','45678',2,1,'default.png','2020-10-18 00:14:47',NULL),(24,787878,'QK-024','N1lRZHc0bU45TlR5UVh6ZVFoMElydz09','Qqqq','Kkk','qk@hotmail.com','76575756',1,1,'default.png','2020-10-18 00:15:47',NULL);
+INSERT INTO `table_user` VALUES (1,14607920,'we21','SWRrSEFnUHpvYm1kRVg4bVBjTEZKQT09','William Enrique','Infante Parra','william@gmail.com','4125181629',1,1,NULL,'2020-10-08 22:11:07',NULL);
 /*!40000 ALTER TABLE `table_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -281,7 +253,7 @@ CREATE TABLE `table_user_rol` (
   `user_nick` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
   `id_rol` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_usuario_rol`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -290,7 +262,7 @@ CREATE TABLE `table_user_rol` (
 
 LOCK TABLES `table_user_rol` WRITE;
 /*!40000 ALTER TABLE `table_user_rol` DISABLE KEYS */;
-INSERT INTO `table_user_rol` VALUES (1,'we21',1),(2,'migue',2),(3,'ybet1',3);
+INSERT INTO `table_user_rol` VALUES (1,'we21',1);
 /*!40000 ALTER TABLE `table_user_rol` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -418,4 +390,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-21 14:49:55
+-- Dump completed on 2020-10-24 11:45:14
